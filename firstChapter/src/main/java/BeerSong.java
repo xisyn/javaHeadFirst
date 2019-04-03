b23b534370afaa123ab4f9ee09a91c8b7188ba53
Dmitriy Hitsunov <dkhitsunov@issart.com>
2019-04-03 16:09:36 +0700

    first commit

---
+++ firstChapter/src/main/java/BeerSong.java
@@ -0,0 +1,10 @@
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+
+        while (beerNum > 0) {
+            
+        }
+    }
+}
